91c887bb71461cf6bda04983840a265d02768c16
Sudheer kumar <sudheerkumar121@gmail.com>
2025-02-04 17:48:11 +0530

    code commit

--- src/components/displaydata.js
+++ src/components/displaydata.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const DisplayData = React.memo(({ post, deletePost }) => {
+const DisplayData = React.memo(({ data, deleteData }) => {
   return (
     <div >
-      <h3>{post.title}</h3>
-      <p>{post.body}</p>
-      <button onClick={() => deletePost(post.id)}>Delete</button>
+      <h3>{data.title}</h3>
+      <p>{data.body}</p>
+      <button onClick={() => deleteData(data.id)}>Delete</button>
     </div>
   );
 });
--- src/components/listData.js
+++ src/components/listData.js
@@ -3,15 +3,15 @@ import { DataContext } from './datacontext';
 import DisplayData from './displaydata';
 
 const ListData = () => {
-  const { data, deletePost } = useContext(DataContext);
+  const { data, deleteData } = useContext(DataContext);
  console.log('data', data);
   return (
     <div>
       {data.length === 0 ? (
         <p>Loading posts...</p>
       ) : (
-        data.map((post) => (
-          <DisplayData key={post.id} post={post} deletePost={deletePost} />
+        data.map((keyData) => (
+          <DisplayData key={keyData.id} data={keyData} deleteData={deleteData} />
         ))
       )}
     </div>
